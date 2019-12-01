63dd70816448114eb34a1ad5d3b2cb0d366c5337
Alex Whitler <alex.whitler@gmail.com>
2019-12-01 11:31:23 -0500

    Day 1 - Part 2

--- day1/main.go
+++ day1/main.go
@@ -11,15 +11,34 @@ func main() {
 	file, _ := os.Open("./input1.txt")
 	scanner := bufio.NewScanner(file)
 
-	sum := 0
+	sum1 := 0
+	sum2 := 0
 	for scanner.Scan() {
-		value, _ := strconv.Atoi(scanner.Text())
-		sum += calcModuleFuel(value)
+		mass, _ := strconv.Atoi(scanner.Text())
+		sum1 += getModFuel1(mass)
+		sum2 += getModFuel2(mass)
 	}
 
-	log.Printf("Part 1: %d", sum)
+	log.Printf("Part 1: %d", sum1)
+	log.Printf("Part 2: %d", sum2)
 }
 
-func calcModuleFuel(value int) int {
-	return (value / 3) - 2
+func getModFuel1(mass int) int {
+	return calcFuel(mass)
+}
+
+func getModFuel2(mass int) int {
+	totalFuel := calcFuel(mass)
+
+	fuel := calcFuel(totalFuel)
+	for fuel > 0 {
+		totalFuel += fuel
+		fuel = calcFuel(fuel)
+	}
+
+	return totalFuel
+}
+
+func calcFuel(mass int) int {
+	return (mass / 3) - 2
 }
