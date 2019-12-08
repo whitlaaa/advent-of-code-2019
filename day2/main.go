08dd407a1888e6bfdb9a3c54440ba50ddbb4c4b7
Alex Whitler <alex.whitler@gmail.com>
2019-12-08 17:52:39 -0500

    Initial commit for Day 5
    * Breaking out intcode to its own package
    * Placeholders for Store and Read opcodes
    * Some minor refactoring ahead of actual day 5 changes

--- day2/main.go
+++ day2/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"advent-of-code-2019/intcode"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"strconv"
-	"strings"
 )
 
 func main() {
@@ -15,54 +14,15 @@ func main() {
 	}
 
 	// Part 1
-	fmt.Println("Part 1: ", runProgram(bytes, 12, 2))
+	fmt.Println("Part 1: ", intcode.RunProgram(bytes, 12, 2))
 
 	// Part 2
-	originalResult := runProgram(bytes, 0, 0)
+	originalResult := intcode.RunProgram(bytes, 0, 0)
 	const targetResult int = 19690720
-	nounWeight := runProgram(bytes, 1, 0) - originalResult
-	verbWeight := runProgram(bytes, 0, 1) - originalResult
+	nounWeight := intcode.RunProgram(bytes, 1, 0) - originalResult
+	verbWeight := intcode.RunProgram(bytes, 0, 1) - originalResult
 	noun := (targetResult - originalResult) / nounWeight
 	verb := ((targetResult - originalResult) % noun) / verbWeight
 
 	fmt.Printf("Part 2: noun = %d, verb = %d, answer = %d\n", noun, verb, (100*noun)+verb)
-}
-
-func runProgram(bytes []byte, noun int, verb int) int {
-	program := getProgram(strings.Split(string(bytes), ","))
-	length := len(program)
-
-	program[1] = noun
-	program[2] = verb
-
-	for i := 0; i < length; i += 4 {
-		opCode := program[i]
-		input1 := program[program[i+1]]
-		input2 := program[program[i+2]]
-		outputPos := program[i+3]
-
-		var opResult int
-		if opCode == 1 {
-			opResult = input1 + input2
-		} else if opCode == 2 {
-			opResult = input1 * input2
-		} else if opCode == 99 {
-			break
-		} else {
-			log.Fatalln("Invalid opcode: ", opCode)
-		}
-
-		program[outputPos] = opResult
-	}
-
-	return program[0]
-}
-
-func getProgram(input []string) []int {
-	var intProgram = []int{}
-	for _, v := range input {
-		value, _ := strconv.Atoi(v)
-		intProgram = append(intProgram, value)
-	}
-	return intProgram
 }
---
+++ intcode/intcode.go
@@ -0,0 +1,65 @@
+package intcode
+
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+const (
+	Add = iota + 1
+	Multiply
+	Store
+	Read
+	Halt = 99
+)
+
+const (
+	Position = iota
+	Immediate
+)
+
+func RunProgram(bytes []byte, noun int, verb int) int {
+	program := formatInput(strings.Split(string(bytes), ","))
+
+	program[1] = noun
+	program[2] = verb
+
+	for i := 0; i < len(program); {
+		opCode := program[i]
+		outputPos := program[i+3]
+
+		var opResult int
+		switch opCode {
+		case Add:
+			opResult = program[program[i+1]] + program[program[i+2]]
+			program[outputPos] = opResult
+			i += 4
+			break
+		case Multiply:
+			opResult = program[program[i+1]] * program[program[i+2]]
+			program[outputPos] = opResult
+			i += 4
+			break
+		case Store:
+			log.Println("Store operation")
+		case Read:
+			log.Println("Read operation")
+		case Halt:
+			return program[0]
+		default:
+			log.Panicln("Invalid op code: ", opCode)
+		}
+	}
+
+	return program[0]
+}
+
+func formatInput(input []string) []int {
+	var intProgram = []int{}
+	for _, v := range input {
+		value, _ := strconv.Atoi(v)
+		intProgram = append(intProgram, value)
+	}
+	return intProgram
+}
